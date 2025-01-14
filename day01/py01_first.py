bf793d31ffa224c0603b8f83d22ffabb77f903da
chs491 <hyunsoo1702@gmail.com>
2025-01-14 16:11:57 +0900

    마무리 정리

--- day01/py01_first.py
+++ day01/py01_first.py
@@ -1,4 +1,5 @@
-# IOT과정 첫번째 프로그램파일
+# IOT과정 첫번째 프로그램파일 - 주석
 
-if __name__ == '__main__':
-    print("Hello Python!")
+if __name__ == '__main__': # 조건문, '__name__' 이 '__main__'이 같다면.
+    print("Hello, Python!") # Hello, Python!을 print() 함수로 실행하라.
+    
